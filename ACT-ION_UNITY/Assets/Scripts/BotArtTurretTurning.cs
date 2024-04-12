341ad3ab5c5736c033a9c169c0bd1d04e5c48692
Hadrnn <timvasilenko2002@gmail.com>
2024-04-12 17:54:57 +0300

    Bot Art

---
+++ ACT-ION_UNITY/Assets/Scripts/BotArtTurretTurning.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotArtTurretTurning : BotTurretTurning
+{
+    //new public Transform FireTransform;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Gun = gameObject.GetComponentInParent<BotShooting>();
+
+        teamNumber = gameObject.GetComponentInParent<TankMovement>().teamNumber;
+
+        collector = GameObject.Find("InfoCollector").GetComponent<InfoCollector>();
+
+        BotTransform = gameObject.GetComponentInParent<TankMovement>().gameObject.transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
