ea516d69ca8767442aa2b9057ddf53b0c91163b3
Hadrnn <timvasilenko2002@gmail.com>
2024-04-12 22:03:18 +0300

    Neural tank added to tank class structure

--- ACT-ION_UNITY/Assets/Scripts/NeuralTankMovement.cs
+++ ACT-ION_UNITY/Assets/Scripts/NeuralTankMovement.cs
@@ -3,24 +3,8 @@ using Unity.Netcode;
 using System.Linq;
 using UnityEngine.Windows;
 
-public class NeuralTankMovement : MonoBehaviour
+public class NeuralTankMovement : TankMovement
 {
-    public float m_Speed = 12f;                 // How fast the tank moves forward and back.
-    public float m_TurnSpeed = 300f;            // How fast the tank turns in degrees per second.
-    public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
-    public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
-    public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
-    public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
-    public int forvard_multiplyer = 1;
-    public int teamNumber = 0;
-
-    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-    private float m_VerticalInputValue;         // The current value of the movement input.
-    private float m_HorizontalInputValue;             // The current value of the turn input.
-    private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
-    private BoxCollider m_Collider;
-
-
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
@@ -52,7 +36,7 @@ public class NeuralTankMovement : MonoBehaviour
     {
         // Add tank object to InfoCollector
         InfoCollector collector = GameObject.Find("InfoCollector").GetComponent<InfoCollector>();
-        collector.teams[teamNumber].tanks.Add(gameObject);
+        collector.AddTank(gameObject);
 
         // Store the original pitch of the audio source.
         m_OriginalPitch = m_MovementAudio.pitch;
@@ -68,35 +52,6 @@ public class NeuralTankMovement : MonoBehaviour
         //Debug.LogWarning(m_MovementInputValue);
         EngineAudio();
     }
-
-
-    private void EngineAudio()
-    {
-        // If there is no input (the tank is stationary)...
-        if (Mathf.Abs(m_VerticalInputValue) < 0.1f && Mathf.Abs(m_HorizontalInputValue) < 0.1f)
-        {
-            // ... and if the audio source is currently playing the driving clip...
-            if (m_MovementAudio.clip == m_EngineDriving)
-            {
-                // ... change the clip to idling and play it.
-                m_MovementAudio.clip = m_EngineIdling;
-                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                m_MovementAudio.Play();
-            }
-        }
-        else
-        {
-            // Otherwise if the tank is moving and if the idling clip is currently playing...
-            if (m_MovementAudio.clip == m_EngineIdling)
-            {
-                // ... change the clip to driving and play.
-                m_MovementAudio.clip = m_EngineDriving;
-                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                m_MovementAudio.Play();
-            }
-        }
-    }
-
 
     private void FixedUpdate()
     {
